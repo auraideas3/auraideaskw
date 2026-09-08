ca9a1a68556c8ab28278cf1e9f7fec0ce4ecba04
auraideas3 <mc.afbm@gmail.com>
2026-09-08 09:44:26 +0400

    Add files via upload

---
+++ ai_studio_code.sh
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "feat: complete Aura Ideas platform with academic branding"
+git branch -M main
+git remote add origin https://github.com/auraideas3/auraideaskw.git
+git push -u origin main --force
